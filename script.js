fe468f44e4c53acb130f47713765d0746cac9bec
asistenciaorienta <asistenciaorienta.gr.sae@juntadeandalucia.es>
2025-09-08 14:19:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -623,7 +623,7 @@ function iniciarAvatarLive2D() {
     window.avatarTalking = () => { talking = true; };
     window.avatarSilencio = () => { talking = false; };
     resolve();
-    })catch(err => {
+    }).catch(err => {
         console.error("Error al cargar el modelo:", err);
         reject(err);
       }
