668f3790dbf83cb9fbb33ffcb5989d0b4485e55f
asistenciaorienta <asistenciaorienta.gr.sae@juntadeandalucia.es>
2025-09-15 13:08:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -659,7 +659,7 @@ async function responderAyuda(necesitaAyuda) {
         texto.classList.add("ocultoDeslizado"); 
         activarReactivacionAvatar(); // ✅ registrar el clic solo después de hablar
         // ✅ Mover avatar a la esquina superior izquierda
-        moverAvatar(20, 10);
+        moverAvatar(5, 5);
         minimizarAvatar(0.3);
       });
   }
